cab49895c4989d35447aadec25dd549502c4139e
Artem Akhmadulin <147542825+aahmadulin@users.noreply.github.com>
2025-02-26 22:47:29 +0300

    Update source_file.py

--- source_file.py
+++ source_file.py
@@ -1,38 +1,23 @@
-# Data processing libs.
 import numpy as np
 import xarray as xr
+import os
 
-# Visualisation libs.
-import matplotlib.pyplot as plt
-import cartopy.crs as ccrs
-import cartopy.feature as cfeature
+# Константы
+PACIFIC_Y_INDICES = (7550, -2)
+ATLANTIC_Y_INDICES = (7350, -1)
+PACIFIC_X_INDICES = (1500, 5900)
+TARGET_PATH = '/mnt/localssd/Data_nemo/Meshes_domains/Coordinates/Regional'
+TARGET_NAME = 'arct_cutorca36_coord.nc'
 
-# Set parameters.
-# Parent coordinate file.
-pcf = xr.open_dataset('/mnt/localssd/Data_nemo/Meshes_domains/Coordinates/Global/ORCA_R36_coord_new.nc').squeeze()
-x_middle = int(pcf['x'].size/2)
-
-# Enter Pacific and Atlantic y-indicies (latitude-like).
-pac_first_yind = 7550
-pac_last_yind = -2
-atl_first_yind = 7350
-atl_last_yind = -1
-
-# Enter first and last x-indicies (latitude-like). It must be less than 1/2 x-dimention size! 
-pac_first_xind = 1500
-pac_last_xind = 5900
-atl_first_xind = pac_last_xind + (x_middle - pac_last_xind)*2 + 1
-atl_last_xind = pcf['x'].size - pac_first_xind + 1
-
-# Saving properties
-target_path = '/mnt/localssd/Data_nemo/Meshes_domains/Coordinates/Regional'
-target_name = 'arct_cutorca36_coord.nc'
-
-# Patch processing
+# grid type lists
+T_VARS = ['nav_lon', 'nav_lat', 'glamt', 'gphit', 'e1t', 'e2t']
+U_VARS = ['glamu', 'gphiu', 'e1u', 'e2u']
+V_VARS = ['glamv', 'gphiv', 'e1v', 'e2v']
+F_VARS = ['glamf', 'gphif', 'e1f', 'e2f']
 
 def grid_selector(pcf, var, extent, pac_patch=False):
     '''
-    Functon that select and cut 2D arrays from parent global ORCA coordinate file.
+    Function that selects and cuts 2D arrays from parent global ORCA coordinate file.
 
     Parameters
     ----------
@@ -41,8 +26,8 @@ def grid_selector(pcf, var, extent, pac_patch=False):
     var : str
         Grid variable name from pcf.
     extent : list
-        List with indicies to cut [y_min, y_max, x_min, x_max].
-    atl_patch : bool
+        List with indices to cut [y_min, y_max, x_min, x_max].
+    pac_patch : bool
         Pacific (True) and Atlantic (False) switch (default is False)
 
     Returns
@@ -50,83 +35,47 @@ def grid_selector(pcf, var, extent, pac_patch=False):
     grid_array
         Ndarray to put in patch dataset.
     '''
+    if var not in pcf:
+        raise ValueError(f"Variable {var} not found in the dataset.")
 
-    # grid type lists
-    t_vars = ['nav_lon', 'nav_lat', 'glamt', 'gphit', 'e1t', 'e2t']
-    u_vars = ['glamu', 'gphiu', 'e1u', 'e2u']
-    v_vars = ['glamv', 'gphiv', 'e1v', 'e2v']
-    f_vars = ['glamf', 'gphif', 'e1f', 'e2f']
-    
     if pac_patch:  # Pacific patch selection
-        if var in t_vars:
+        if var in T_VARS:
             grid_array = np.flip(pcf[var].sel(y=slice(extent[0], extent[1]), x=slice(extent[2], extent[3])).values)
-        elif var in u_vars:
+        elif var in U_VARS:
             grid_array = np.flip(pcf[var].sel(y=slice(extent[0], extent[1]), x=slice(extent[2]-1, extent[3]-1)).values)
-        elif var in v_vars:
+        elif var in V_VARS:
             grid_array = np.flip(pcf[var].sel(y=slice(extent[0]-1, extent[1]-1), x=slice(extent[2], extent[3])).values)
-        elif var in f_vars:
+        elif var in F_VARS:
             grid_array = np.flip(pcf[var].sel(y=slice(extent[0]-1, extent[1]-1), x=slice(extent[2]-1, extent[3]-1)).values)
-    else:  #  Atlantic patch selection
+    else:  # Atlantic patch selection
         grid_array = pcf[var].sel(y=slice(extent[0], extent[1]), x=slice(extent[2], extent[3])).values
-
-    # TODO: There may be var name error handler like "There no variable named {var} in parent coordinate file".
 
     return grid_array
 
-# Dataset creation
-# TODO: dataset generator. I don't like this wet shit.
+# Функция создания датасета
+def create_dataset(pcf, extent, pac_patch=False):
+    data_vars = {}
+    for var in T_VARS + U_VARS + V_VARS + F_VARS:
+        data_vars[var] = (["y", "x"], grid_selector(pcf, var, extent, pac_patch))
+    return xr.Dataset(data_vars)
 
-# Atlantic patch as xarray Dataset
-atl_extent = [atl_first_yind, atl_last_yind, atl_first_xind, atl_last_xind]
-atl_dataset = xr.Dataset(
-    data_vars=dict(
-        nav_lon=(["y", "x"], grid_selector(pcf, 'nav_lon', atl_extent)),
-        nav_lat=(["y", "x"], grid_selector(pcf, 'nav_lat', atl_extent)),
-        glamt=(["y", "x"], grid_selector(pcf, 'glamt', atl_extent)),
-        glamu=(["y", "x"], grid_selector(pcf, 'glamu', atl_extent)),
-        glamv=(["y", "x"], grid_selector(pcf, 'glamv', atl_extent)),
-        glamf=(["y", "x"], grid_selector(pcf, 'glamf', atl_extent)),
-        gphit=(["y", "x"], grid_selector(pcf, 'gphit', atl_extent)),
-        gphiu=(["y", "x"], grid_selector(pcf, 'gphiu', atl_extent)),
-        gphiv=(["y", "x"], grid_selector(pcf, 'gphiv', atl_extent)),
-        gphif=(["y", "x"], grid_selector(pcf, 'gphif', atl_extent)),
-        e1t=(["y", "x"], grid_selector(pcf, 'e1t', atl_extent)),
-        e1u=(["y", "x"], grid_selector(pcf, 'e1u', atl_extent)),
-        e1v=(["y", "x"], grid_selector(pcf, 'e1v', atl_extent)),
-        e1f=(["y", "x"], grid_selector(pcf, 'e1f', atl_extent)),
-        e2t=(["y", "x"], grid_selector(pcf, 'e2t', atl_extent)),
-        e2u=(["y", "x"], grid_selector(pcf, 'e2u', atl_extent)),
-        e2v=(["y", "x"], grid_selector(pcf, 'e2v', atl_extent)),
-        e2f=(["y", "x"], grid_selector(pcf, 'e2f', atl_extent))
-    )
-)
+def main():
+    pcf = xr.open_dataset('/mnt/localssd/Data_nemo/Meshes_domains/Coordinates/Global/ORCA_R36_coord_new.nc').squeeze()
+    x_middle = int(pcf['x'].size / 2)
 
-# Pacific patch as xarray Dataset
-pac_extent = [pac_first_yind, pac_last_yind, pac_first_xind, pac_last_xind]
-pac_dataset = xr.Dataset(
-    data_vars=dict(
-        nav_lon=(["y", "x"], grid_selector(pcf, 'nav_lon', pac_extent, pac_patch=True)),
-        nav_lat=(["y", "x"], grid_selector(pcf, 'nav_lat', pac_extent, pac_patch=True)),
-        glamt=(["y", "x"], grid_selector(pcf, 'glamt', pac_extent, pac_patch=True)),
-        glamu=(["y", "x"], grid_selector(pcf, 'glamu', pac_extent, pac_patch=True)),
-        glamv=(["y", "x"], grid_selector(pcf, 'glamv', pac_extent, pac_patch=True)),
-        glamf=(["y", "x"], grid_selector(pcf, 'glamf', pac_extent, pac_patch=True)),
-        gphit=(["y", "x"], grid_selector(pcf, 'gphit', pac_extent, pac_patch=True)),
-        gphiu=(["y", "x"], grid_selector(pcf, 'gphiu', pac_extent, pac_patch=True)),
-        gphiv=(["y", "x"], grid_selector(pcf, 'gphiv', pac_extent, pac_patch=True)),
-        gphif=(["y", "x"], grid_selector(pcf, 'gphif', pac_extent, pac_patch=True)),
-        e1t=(["y", "x"], grid_selector(pcf, 'e1t', pac_extent, pac_patch=True)),
-        e1u=(["y", "x"], grid_selector(pcf, 'e1u', pac_extent, pac_patch=True)),
-        e1v=(["y", "x"], grid_selector(pcf, 'e1v', pac_extent, pac_patch=True)),
-        e1f=(["y", "x"], grid_selector(pcf, 'e1f', pac_extent, pac_patch=True)),
-        e2t=(["y", "x"], grid_selector(pcf, 'e2t', pac_extent, pac_patch=True)),
-        e2u=(["y", "x"], grid_selector(pcf, 'e2u', pac_extent, pac_patch=True)),
-        e2v=(["y", "x"], grid_selector(pcf, 'e2v', pac_extent, pac_patch=True)),
-        e2f=(["y", "x"], grid_selector(pcf, 'e2f', pac_extent, pac_patch=True))
-    ),
-)
+    atl_first_xind = PACIFIC_X_INDICES[1] + (x_middle - PACIFIC_X_INDICES[1]) * 2 + 1
+    atl_last_xind = pcf['x'].size - PACIFIC_X_INDICES[0] + 1
 
-whole_dataset = xr.concat([atl_dataset, pac_dataset], dim='y')
-whole_dataset.to_netcdf(f'{target_path}/{target_name}')
+    atl_extent = [ATLANTIC_Y_INDICES[0], ATLANTIC_Y_INDICES[1], atl_first_xind, atl_last_xind]
+    pac_extent = [PACIFIC_Y_INDICES[0], PACIFIC_Y_INDICES[1], PACIFIC_X_INDICES[0], PACIFIC_X_INDICES[1]]
 
-# TODO: Some visualization?
+    atl_dataset = create_dataset(pcf, atl_extent)
+    pac_dataset = create_dataset(pcf, pac_extent, pac_patch=True)
+
+    whole_dataset = xr.concat([atl_dataset, pac_dataset], dim='y')
+    if not os.path.exists(TARGET_PATH):
+        os.makedirs(TARGET_PATH)
+    whole_dataset.to_netcdf(f'{TARGET_PATH}/{TARGET_NAME}')
+
+if __name__ == "__main__":
+    main() # Точка входа для запуска скрипта как программы
